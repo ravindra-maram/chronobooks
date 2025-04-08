561244ff280632288eb4a2bb1fe70a9f06154762
ravindra-maram <Rmaram0823@conestogac.on.ca>
2025-04-07 22:12:34 -0400

    Add AudiobookRepository to combine data and network layers

--- app/src/main/java/com/example/chronobooks/utils/ThemeUtils.java
+++ app/src/main/java/com/example/chronobooks/utils/ThemeUtils.java
@@ -1,4 +1,16 @@
 package com.example.chronobooks.utils;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+
 public class ThemeUtils {
+
+    public static int getThemeColor(Context context, int resId) {
+        return context.getResources().getColor(resId, context.getTheme());
+    }
+
+    public static int dpToPx(int dp) {
+        return Math.round(dp * Resources.getSystem().getDisplayMetrics().density);
+    }
 }
