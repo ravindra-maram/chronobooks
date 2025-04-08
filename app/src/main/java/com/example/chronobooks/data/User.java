bbe00867dd67297f52e559713519b1c2a83accbf
ravindra-maram <Rmaram0823@conestogac.on.ca>
2025-04-07 21:40:38 -0400

    Add data models: Audiobook, Chapter, User

--- app/src/main/java/com/example/chronobooks/data/User.java
+++ app/src/main/java/com/example/chronobooks/data/User.java
@@ -1,4 +1,14 @@
 package com.example.chronobooks.data;
 
 public class User {
+    private String username;
+    private String token;
+
+    public User(String username, String token) {
+        this.username = username;
+        this.token = token;
+    }
+
+    public String getUsername() { return username; }
+    public String getToken() { return token; }
 }
