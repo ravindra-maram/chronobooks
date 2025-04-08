8288e608feb7afd62f20dfe3def2b5ab53901362
ravindra-maram <Rmaram0823@conestogac.on.ca>
2025-04-07 21:03:07 -0400

    Complete UI setup: added MainActivity, PlayerActivity, AudiobookListFragment, layouts

--- app/src/main/java/com/example/chronobooks/ui/AudiobookListFragment.java
+++ app/src/main/java/com/example/chronobooks/ui/AudiobookListFragment.java
@@ -1,4 +1,32 @@
 package com.example.chronobooks.ui;
 
-public class AudiobookListFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.chronobooks.R;
+import com.example.chronobooks.ui.components.AudiobookCardView;
+
+public class AudiobookListFragment extends Fragment {
+
+    public AudiobookListFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_audiobook_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewAudiobooks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new AudiobookCardView());
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/chronobooks/ui/components/AudiobookCardView.java
+++ app/src/main/java/com/example/chronobooks/ui/components/AudiobookCardView.java
@@ -1,4 +1,41 @@
 package com.example.chronobooks.ui.components;
 
-public class AudiobookCardView {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.chronobooks.R;
+
+public class AudiobookCardView extends RecyclerView.Adapter<AudiobookCardView.ViewHolder> {
+
+    private final String[] mockData = {"Book One", "Book Two", "Book Three"};
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_audiobook_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(mockData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mockData.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public ViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.tvAudiobookTitle);
+        }
+    }
 }
